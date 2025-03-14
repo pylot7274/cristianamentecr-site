c3e0653836c064a45f8334dc4382bf6794fb06a8
Arthur <useforjobsonly@gmail.com>
2025-03-14 14:37:18 -0600

    use templateview for simple home display

--- cristianamentecr/cristianamente/home_landing/views.py
+++ cristianamentecr/cristianamente/home_landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class HomeLandingPage(TemplateView):
+    """Home/Landing page."""
+    template_name = "home_landing.html"
