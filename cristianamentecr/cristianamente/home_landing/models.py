71d6c34e81d616845be151fcac4f2d865f07a641
Arthur <useforjobsonly@gmail.com>
2025-03-14 14:36:55 -0600

    create a home page model for dynamic content

--- cristianamentecr/cristianamente/home_landing/models.py
+++ cristianamentecr/cristianamente/home_landing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class HomePage(models.Model):
+    """Home/Landing page content.
+       Using model approach in case dynamic content
+       needs to be generated.
+    """
+    site_name = models.CharField(max_length=250)
+    logo_image = models.ImageField(
+        upload_to="home_landing/img", editable=False, default='cristianamente_logo.png')
+    tagline = models.CharField(max_length=250)
+    briefing = models.TextField()
